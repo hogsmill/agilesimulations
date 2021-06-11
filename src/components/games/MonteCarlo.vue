95e56f4743ae7662491199b7ad4034d14a3f2954
hogsmill <info@hogsmill.com>
2021-06-11 20:01:00 +0100

    more pages

--- src/components/games/MonteCarlo.vue
+++ src/components/games/MonteCarlo.vue
@@ -1,5 +1,47 @@
 <template>
-  <div>
-    Monto Carlo
+  <div class="monte-carlo">
+    <h2>
+      Monte Carlo Simulations
+    </h2>
+    <div class="row">
+      <div class="col-sm-4">
+        <p>
+          TBD
+        </p>
+      </div>
+      <div class="col-sm-8">
+        <div class="pic" />
+      </div>
+    </div>
   </div>
 </template>
+
+  <script>
+  import bus from '../../socket.js'
+
+  export default {
+    methods: {
+      contact() {
+        bus.$emit('contact', {})
+      }
+    }
+  }
+  </script>
+
+<style lang="scss">
+  .monte-carlo {
+    padding: 0 36px;
+
+    h2 {
+      text-align: center;
+    }
+
+    .pic {
+      margin: 0 24px 24px 24px;
+      background-image: url("../../assets/img/screenshots/monte-carlo.jpg");
+      background-size: contain;
+      background-repeat: no-repeat;
+      height: 400px;
+    }
+  }
+</style>
--- src/components/games/NoEstimatesMobile.vue
+++ src/components/games/NoEstimatesMobile.vue
@@ -1,5 +1,47 @@
 <template>
-  <div>
-    No Estimates Mobile
+  <div class="no-estimates-mobile">
+    <h2>
+      No Estimates Mobile
+    </h2>
+    <div class="row">
+      <div class="col-sm-4">
+        <p>
+          TBD
+        </p>
+      </div>
+      <div class="col-sm-8">
+        <div class="pic" />
+      </div>
+    </div>
   </div>
 </template>
+
+  <script>
+  import bus from '../../socket.js'
+
+  export default {
+    methods: {
+      contact() {
+        bus.$emit('contact', {})
+      }
+    }
+  }
+  </script>
+
+<style lang="scss">
+  .no-estimates-mobile {
+    padding: 0 36px;
+
+    h2 {
+      text-align: center;
+    }
+
+    .pic {
+      margin: 0 24px 24px 24px;
+      background-image: url("../../assets/img/screenshots/no-estimates-mobile.jpg");
+      background-size: contain;
+      background-repeat: no-repeat;
+      height: 400px;
+    }
+  }
+</style>
