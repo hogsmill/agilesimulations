b361ea4fdcf3063158dcc55f1f341aa7bd225f31
hogsmill <info@hogsmill.com>
2021-08-07 18:07:21 +0100

    sitemap

--- src/store/lib/timeFuns.js
+++ src/store/lib/timeFuns.js
@@ -2,20 +2,8 @@
 module.exports = {
 
   w3cDate: function() {
-    const date = new Date()
-    let y = date.getYear() + 1900
-    let m = date.getMonth()
-    if (m < 10) {
-      m = '0' + m
-    }
-    let d = date.getDay()
-    if (d < 10) {
-      d = '0' + d
-    }
-    const hh = date.getHours()
-    const mm = date.getMinutes()
-    const ss = date.getSeconds()
-    let dateStr = y + '-' + m + '-' + d + 'T' + hh + ':' + mm + ':' + ss
+    const d = new Date();
+    const dateStr = d.toISOString();
     return dateStr
   }
 }
